4229749a1450aa9b485b116a97c10180deffdf8e
Md Adil <122589915+eyeaadil@users.noreply.github.com>
2025-07-01 10:14:34 +0530

    [3330] [Time Beats: 100.00%] [Memory Beats: 60.22%] - LeetPush

---
+++ graph/daily-problem/Find-the-Original-Typed-String-I.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int n = word.size();
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            if(word[i] == word[i+1]){
+                cnt++;
+            }
+        }
+
+        return cnt+1;
+    }
+};
